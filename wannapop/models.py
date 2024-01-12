d95f00472d57c13663bea6870ce47baf42282eec
Adrià Martínez <amartinezp@fp.insjoaquimmir.cat>
2024-01-12 16:44:18 +0100

    Clases mixin

--- wannapop/models.py
+++ wannapop/models.py
@@ -2,8 +2,9 @@ from . import db_manager as db
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin
+from .mixins import BaseMixin, SerializableMixin
 
-class Product(db.Model):
+class Product(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -17,13 +18,13 @@ class Product(db.Model):
     banned = relationship("BannedProduct", backref="product", uselist=False)
 
 
-class Category(db.Model):
+class Category(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     slug = db.Column(db.String, nullable=False)
 
-class User(UserMixin, db.Model):
+class User(db.Model,UserMixin , BaseMixin, SerializableMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, unique=True)
@@ -35,14 +36,18 @@ class User(UserMixin, db.Model):
     email_token = db.Column(db.String(20))
     verified = db.Column(db.Integer, default=0, nullable=False)
     blocked = relationship("BlockedUser", backref="user", uselist=False)
+     # Class variable from SerializableMixin
+    exclude_attr = ['password']
+    def get_id(self):
+        return self.email
 
-class BlockedUser(db.Model):
+class BlockedUser(db.Model , BaseMixin, SerializableMixin):
     __tablename__ = "blocked_users"
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     message = db.Column(db.String, nullable=False)
     created = db.Column(db.DateTime, server_default=func.now())
 
-class BannedProduct(db.Model):
+class BannedProduct(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "banned_products"
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"), primary_key=True)
     reason = db.Column(db.String, nullable=False)
